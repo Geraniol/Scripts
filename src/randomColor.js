8bfb92e867faac674c3561c4d3f275eb58c78248
Geraniol <1215918624@qq.com>
2022-12-03 11:00:48 +0800

    2022 1203 1101

--- src/randomColor.js
+++ src/randomColor.js
@@ -1,5 +1,6 @@
 // String <= randomColor(Boolean <= true, Boolean <= false);
 // 生成随机颜色 <= 是否为颜色名添加井号，是否生成透明通道
+
 function randomColor(addNumberSign = true, enableAlphaChannel = false) {
     return { true: "#", false: "" }[addNumberSign] + {
         true: Math.floor(Math.random() * 0xffffffff).toString(16),
